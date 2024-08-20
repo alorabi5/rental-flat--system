25e3cdf7668b81d44c201ee387d362baf615756c
StoicTylerDurden <buhussayyen@gmail.com>
2024-08-20 10:18:03 +0300

    Removed Dead Code

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const testJWTRouter = require("./controllers/test-jwt");
 const usersRouter = require("./controllers/users");
 const profilesRouter = require("./controllers/profiles");
 const flatRouter = require("./controllers/flat");
@@ -20,7 +19,6 @@ app.use(express.json());
 
 // Routes go here
 app.use(morgan("dev"));
-app.use("/test-jwt", testJWTRouter);
 app.use("/users", usersRouter);
 app.use("/profiles", profilesRouter);
 app.use("/flat", flatRouter);
